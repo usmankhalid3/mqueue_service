9d8c673380aa6c4acfb88d96dce59b10909d0072
Usman Khalid <usman@favoritemedium.com>
2016-10-29 22:43:34 +0500

    Updating default visibility timeout to 10 secs

--- src/main/java/com/example/common/Constants.java
+++ src/main/java/com/example/common/Constants.java
@@ -2,7 +2,7 @@ package com.example.common;
 
 public class Constants {
 
-	public static final int defaultVisibilityTimeout = 1500; //millis
+	public static final int defaultVisibilityTimeout = 10000; //millis
 	public static final int maxVisibilityTimeout = 345600000; //millis in 4 days
 	public static final int maxRetrievalCount = 10;
 }
